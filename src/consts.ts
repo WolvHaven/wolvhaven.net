50a20bfbf76827d0785ddeb0b698828a2a367524
Qi Xiang <qixyqix@outlook.com>
2023-12-17 02:31:00 +0800

    Changed accent color and changed font to Claity City

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'WolvHaven';
+export const SITE_DESCRIPTION = 'More to Explore - A creative minecraft server for the city-build and transit enthusiast';
