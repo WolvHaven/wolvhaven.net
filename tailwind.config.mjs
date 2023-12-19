cd828403f39cdf1d9304aee6b3445a4258ebef26
Qi Xiang <qixyqix@outlook.com>
2023-12-19 16:09:54 +0800

    Update font in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Nunito Sans", "ClaityCity", ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				main:{
 					50: '#7f0000',
