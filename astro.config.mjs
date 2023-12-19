2fc172770a1674b562a3c089f5311d93d0e9e799
Qi Xiang <qixyqix@outlook.com>
2023-12-19 18:09:50 +0800

    Modify astro config to use custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wolvhaven.github.io',
-  base: '/wolvhaven.net',
+  site: 'https://wolvhaven.net',
   integrations: [mdx(), sitemap(), tailwind()]
 });
