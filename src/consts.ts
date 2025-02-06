13c9767ad6966be7cb1b17a736b88abcac30f632
matthieu1221 <138528318+matthieu1221@users.noreply.github.com>
2025-02-06 00:21:30 +0000

    ContentUpdate: Links to socials and minor typo fixes.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'WolvHaven';
-export const SITE_DESCRIPTION = 'More to Explore - A creative minecraft server for the city-build and transit enthusiast';
+export const SITE_DESCRIPTION = 'More to Explore - A creative Minecraft server for the city-build and transit enthusiast';
