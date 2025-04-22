7629c4de582dfd88eaad10b228d82fc446af19a7
matthieu1221 <138528318+matthieu1221@users.noreply.github.com>
2025-04-22 14:52:52 +0100

    FeatureUpdate: Direct Embeds for YT videos, etc...

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -190,224 +190,224 @@ declare module 'astro:content' {
   slug: "2020-01-13-discord-qr-code";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
-"2020-01-23-wher-year.md": {
-	id: "2020-01-23-wher-year.md";
+"2020-01-23-wher-year.mdx": {
+	id: "2020-01-23-wher-year.mdx";
   slug: "2020-01-23-wher-year";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
+  data: any
+} & { render(): Render[".mdx"] };
 "2020-03-11-citizen-app.md": {
 	id: "2020-03-11-citizen-app.md";
   slug: "2020-03-11-citizen-app";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2020-03-26-elytra-race.md": {
 	id: "2020-03-26-elytra-race.md";
   slug: "2020-03-26-elytra-race";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2020-05-03-113-upgrade.md": {
 	id: "2020-05-03-113-upgrade.md";
   slug: "2020-05-03-113-upgrade";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2020-05-07-reddit.md": {
 	id: "2020-05-07-reddit.md";
   slug: "2020-05-07-reddit";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
-"2020-05-10-may-2020-ge.md": {
-	id: "2020-05-10-may-2020-ge.md";
+"2020-05-10-may-2020-ge.mdx": {
+	id: "2020-05-10-may-2020-ge.mdx";
   slug: "2020-05-10-may-2020-ge";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
-"2020-05-31-may-2020-ge-results.md": {
-	id: "2020-05-31-may-2020-ge-results.md";
+  data: any
+} & { render(): Render[".mdx"] };
+"2020-05-31-may-2020-ge-results.mdx": {
+	id: "2020-05-31-may-2020-ge-results.mdx";
   slug: "2020-05-31-may-2020-ge-results";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
+  data: any
+} & { render(): Render[".mdx"] };
 "2020-06-22-pride-2020.md": {
 	id: "2020-06-22-pride-2020.md";
   slug: "2020-06-22-pride-2020";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
-"2020-06-27-pride-2020-recap.md": {
-	id: "2020-06-27-pride-2020-recap.md";
+"2020-06-27-pride-2020-recap.mdx": {
+	id: "2020-06-27-pride-2020-recap.mdx";
   slug: "2020-06-27-pride-2020-recap";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
+  data: any
+} & { render(): Render[".mdx"] };
 "2020-08-05-server-leadership.md": {
 	id: "2020-08-05-server-leadership.md";
   slug: "2020-08-05-server-leadership";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2020-10-14-political-system-changes.md": {
 	id: "2020-10-14-political-system-changes.md";
   slug: "2020-10-14-political-system-changes";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2020-10-29-staff-changes-whistleblowing.md": {
 	id: "2020-10-29-staff-changes-whistleblowing.md";
   slug: "2020-10-29-staff-changes-whistleblowing";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2020-11-08-representation-referendum.md": {
 	id: "2020-11-08-representation-referendum.md";
   slug: "2020-11-08-representation-referendum";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2021-04-05-new-wiki.md": {
 	id: "2021-04-05-new-wiki.md";
   slug: "2021-04-05-new-wiki";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2021-06-03-new-wiki-register.md": {
 	id: "2021-06-03-new-wiki-register.md";
   slug: "2021-06-03-new-wiki-register";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2021-06-14-changes-server-experience.md": {
 	id: "2021-06-14-changes-server-experience.md";
   slug: "2021-06-14-changes-server-experience";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
-"2021-06-15-pride-2021.md": {
-	id: "2021-06-15-pride-2021.md";
+"2021-06-15-pride-2021.mdx": {
+	id: "2021-06-15-pride-2021.mdx";
   slug: "2021-06-15-pride-2021";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
-"2021-06-21-new-rules.md": {
-	id: "2021-06-21-new-rules.md";
+  data: any
+} & { render(): Render[".mdx"] };
+"2021-06-21-new-rules.mdx": {
+	id: "2021-06-21-new-rules.mdx";
   slug: "2021-06-21-new-rules";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
-"2021-06-26-pride-2021-recap.md": {
-	id: "2021-06-26-pride-2021-recap.md";
+  data: any
+} & { render(): Render[".mdx"] };
+"2021-06-26-pride-2021-recap.mdx": {
+	id: "2021-06-26-pride-2021-recap.mdx";
   slug: "2021-06-26-pride-2021-recap";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
-"2021-10-27-vision-2023.md": {
-	id: "2021-10-27-vision-2023.md";
+  data: any
+} & { render(): Render[".mdx"] };
+"2021-10-27-vision-2023.mdx": {
+	id: "2021-10-27-vision-2023.mdx";
   slug: "2021-10-27-vision-2023";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
+  data: any
+} & { render(): Render[".mdx"] };
 "2022-04-06-vision-2023-rank-structure.md": {
 	id: "2022-04-06-vision-2023-rank-structure.md";
   slug: "2022-04-06-vision-2023-rank-structure";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2023-04-20-introducing-town-hall.md": {
 	id: "2023-04-20-introducing-town-hall.md";
   slug: "2023-04-20-introducing-town-hall";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2023-12-20-2023-new-website.md": {
 	id: "2023-12-20-2023-new-website.md";
   slug: "2023-12-20-2023-new-website";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2024-01-28-townhall-2024-01.md": {
 	id: "2024-01-28-townhall-2024-01.md";
   slug: "2024-01-28-townhall-2024-01";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2024-02-15-calidia-cleanup-2024.md": {
 	id: "2024-02-15-calidia-cleanup-2024.md";
   slug: "2024-02-15-calidia-cleanup-2024";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2024-09-28-mywarp-introduction.md": {
 	id: "2024-09-28-mywarp-introduction.md";
   slug: "2024-09-28-mywarp-introduction";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2024-10-07-townhall-2024-09.md": {
 	id: "2024-10-07-townhall-2024-09.md";
   slug: "2024-10-07-townhall-2024-09";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
-"2025-01-01-trolligarch-documentary.md": {
-	id: "2025-01-01-trolligarch-documentary.md";
+"2025-01-01-trolligarch-documentary.mdx": {
+	id: "2025-01-01-trolligarch-documentary.mdx";
   slug: "2025-01-01-trolligarch-documentary";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
-"2025-02-01-spawn-trailer.md": {
-	id: "2025-02-01-spawn-trailer.md";
+  data: any
+} & { render(): Render[".mdx"] };
+"2025-02-01-spawn-trailer.mdx": {
+	id: "2025-02-01-spawn-trailer.mdx";
   slug: "2025-02-01-spawn-trailer";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
-} & { render(): Render[".md"] };
+  data: any
+} & { render(): Render[".mdx"] };
 "2025-03-11-1214-test-server.md": {
 	id: "2025-03-11-1214-test-server.md";
   slug: "2025-03-11-1214-test-server";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 "2025-04-11-1214-update.md": {
 	id: "2025-04-11-1214-update.md";
   slug: "2025-04-11-1214-update";
   body: string;
   collection: "news";
-  data: InferEntrySchema<"news">
+  data: any
 } & { render(): Render[".md"] };
 };
 
@@ -419,5 +419,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	type ContentConfig = typeof import("../src/content/config");
+	type ContentConfig = never;
 }
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import embeds from 'astro-embed/integration';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wolvhaven.net',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [embeds(), mdx(), sitemap(), tailwind()]
 });
