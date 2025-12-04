29e06a0d200413e62e1a6bf3a4466bcdc3bc2c14
Qi Xiang <qixyqix@outlook.com>
2025-12-05 02:42:13 +0800

    Feat: Upgrade to Astro v5 Tailwind v4
    Squashed commit of the following:
    
    commit 24037aee2d834e24bfbbd804ba48b1099fb50e87
    Author: Qi Xiang <qixyqix@outlook.com>
    Date:   Fri Dec 5 02:40:25 2025 +0800
    
        Fix: li margin
    
    commit d9e483ab75a7f8d4a7c36e6a217ffc191bd1c624
    Author: Qi Xiang <qixyqix@outlook.com>
    Date:   Fri Dec 5 02:39:44 2025 +0800
    
        Fix: list outside
    
    commit 2f736a60ada6aeb85772db328daef9cd61b74e60
    Author: Qi Xiang <qixyqix@outlook.com>
    Date:   Fri Dec 5 02:36:50 2025 +0800
    
        Fix: tailwind v4 adjustments
    
    commit 6b01d25fe55a034977b7b5542fd08eba73cf1ed7
    Author: Qi Xiang <qixyqix@outlook.com>
    Date:   Fri Dec 5 01:42:02 2025 +0800
    
        Feat: Upgrade to tailwind 4
    
    commit ad383bf4fd55618d6ed1d33ef7669da212593a94
    Author: Qi Xiang <qixyqix@outlook.com>
    Date:   Fri Dec 5 01:16:01 2025 +0800
    
        Feat: Upgrade to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import embeds from 'astro-embed/integration';
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wolvhaven.net',
-  integrations: [embeds(), mdx(), sitemap(), tailwind()]
+  integrations: [embeds(), mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
